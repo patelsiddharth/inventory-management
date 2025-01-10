445dee3ca548f8a234419a48e48a5217aba163fb
siddharth patel <siddharthpatel425@gmail.com>
2025-01-10 17:12:57 +0530

    Removed hardcoding

--- src/App.js
+++ src/App.js
@@ -37,45 +37,8 @@ export default function App() {
             try 
             {
                 setLoading(true);
-                // const response = await fetch(URL);
-                // const parsedResponse = await response.json();
-                const parsedResponse = [
-                    {
-                        name: "Bluetooth",
-                        category: "Electronic",
-                        value: "$150",
-                        quantity: 5,
-                        price: "$30",
-                    },
-                    {
-                        name: "Edifier M43560",
-                        category: "Electronic",
-                        value: "0",
-                        quantity: 0,
-                        price: "$0",
-                    },
-                    {
-                        name: "Sony 4k ultra 55 inch TV",
-                        category: "Electronic",
-                        value: "$1190",
-                        quantity: 17,
-                        price: "$70",
-                    },
-                    {
-                        name: "Samsumg 55 inch TV",
-                        category: "Electronic",
-                        value: "$600",
-                        quantity: 50,
-                        price: "$12",
-                    },
-                    {
-                        name: "samsumg S34 Ultra",
-                        category: "phone",
-                        value: "$0",
-                        quantity: 0,
-                        price: "$0",
-                    },
-                ];
+                const response = await fetch(URL);
+                const parsedResponse = await response.json();
                 parsedResponse.forEach((item) => {
                     item.isDisabled = false;
                     const arr = new Uint32Array(1);
